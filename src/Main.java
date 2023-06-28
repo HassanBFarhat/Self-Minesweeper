2b4b7a57bcd4da1438a5774cb447cb979c8d1bee
HassanBFarhat <farhathassan@rocketmail.com>
2023-06-27 19:52:27 -0700

    fifth commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-
-//        System.out.println((int) (Math.random() * 100 + 1));
-
-        MinesweeperMatrixBoarderBuffer borderedField =
-                new MinesweeperMatrixBoarderBuffer(new File("team_minesweeper_input.txt"));
-
-
-
-
+    public static void main(String[] args) throws IOException {
+        MinesweeperHintField mf = new MinesweeperHintField();
+        final Scanner scanner = new Scanner(new File("team_minesweeper_input.txt"));
+        int currentRows;
+        int currentCols;
+        do {
+            currentRows = scanner.nextInt() + 2;
+            currentCols = scanner.nextInt() + 2;
+            if (currentRows == 2 && currentCols == 2) {
+                break;
+            }
+            String[][] inputArr = new String[currentRows][currentCols];
+            mf.addBufferToInputMineField(inputArr, currentRows, currentCols, scanner);
+            mf.generatorHintedMineField(inputArr, currentRows, currentCols);
+        } while (currentRows != 2 && currentCols != 2);
     }
 }
